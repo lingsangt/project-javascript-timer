b259d1323aa27b548aa341879f3ded84f9dc54cc
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-04-27 14:57:23 -0400

    set up timer callbacks

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         //console.log (this)
         this.durationInput = durationInput;
         this.startButton = startButton;
@@ -8,6 +8,16 @@ class Timer {
         //this invokes start method
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener ('click', this.pause);
+
+        /*
+        Here, the callbacks aren't optional, but we use all of them here. It is a good idea though, 
+        in general, to assume they are optional and check if they exist.
+        */
+        if (callbacks){
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
     }
 
 
@@ -17,6 +27,11 @@ class Timer {
     */
     start =() => {
 
+        //Again, it is a good idea to check to make sure onStart was initialized in the constructor before using it.
+        if (this.onStart){
+            this.onStart;
+        }
+
         //we want tick to be called the first time immediately the user clicks the button
         this.tick ();
 
@@ -25,6 +40,7 @@ class Timer {
 
     }
     pause = () => {
+        console.log ('Timer paused!');
 
         /*
         note that setInterval returns an integer. If you want to stop it, you run clearInterval
@@ -38,8 +54,10 @@ class Timer {
     */
     tick = () => {
         
-        if (this.timeRemaining < 0){
+        if (this.timeRemaining <= 0){
+            
             this.pause ();
+            this.onComplete ();
         }
         else {
             /*
@@ -48,6 +66,9 @@ class Timer {
             this.timeRemaining = timeRemaining -1; //Uses setter method to update time
             */
             this.timeRemaining = this.timeRemaining -1;
+            if (this.onTick){
+                this.onTick ();
+            }
         }
 
 
@@ -67,7 +88,31 @@ class Timer {
   const startButton = document.querySelector('#start');
   const pauseButton = document.querySelector('#pause');
   
-  const timer = new Timer(durationInput, startButton, pauseButton);
+  
+  /*
+  Part 3: Updating the border will be more complicated, so it split it into segments.
+  */
+
+ 
+  const timer = new Timer(durationInput, startButton, pauseButton, {
+
+        /*
+        Watch for this event indicating that the timer has started. When it occurs, draw the border.
+        This onStart callback is going to be invovked the instant we start up the timer
+        */
+        onStart(){
+            console.log ('Timer just started!')
+        }, 
+        //Watch for this event indicating that the timer has 'ticked.' When it occurs, update the border.
+        onTick(){
+
+        }, 
+        //Watch for this event indicating that the timer is done. When it occurs, reset the border.
+        onComplete(){
+
+        }
+
+  });
   
 /*
 
