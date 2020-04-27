b7b030377359081d9590e9042ae4c9feb30e6525
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-04-26 22:40:22 -0400

    part 1 with notes

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ class Timer {
       this.startButton = startButton;
       this.pauseButton = pauseButton;
   
+      //this invokes start method
       this.startButton.addEventListener('click', this.start);
     }
   
     start() {
-      console.log('Time to start the timer!');
+        console.log('Time to start the timer!');
+
     }
+
   }
   
   const durationInput = document.querySelector('#duration');
@@ -18,3 +21,13 @@ class Timer {
   
   const timer = new Timer(durationInput, startButton, pauseButton);
   
+  /*
+Note: If we called timer.start () directly, this creates a different instance than 
+when the button is clicked. 
+
+You can check this by adding the line console.log (this) and checking the printouts
+in the console. 
+
+You have to have a much better understanding of what the keyword 'this' inside the 
+function start(). 
+  */
